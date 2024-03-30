4fe90a42d4e0105f32951d10902c940e682dfd42
Mateusz Okulus <mmokulus@gmail.com>
2024-03-30 09:34:06 +0100

    Add basic GraphQL quizz schema

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { QuizzesService } from './quizzes.service';
 import { QuizzesResolver } from './quizzes.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   providers: [QuizzesResolver, QuizzesService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/quizzes/quizzes.graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
 })
 export class QuizzesModule {}
