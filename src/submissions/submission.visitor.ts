391c90ce289d2b2443873f9f5b9bb3b79233d080
Mateusz Okulus <mmokulus@gmail.com>
2024-04-27 13:59:19 +0200

    Remove unused class

--- src/submissions/submission.visitor.ts
+++ src/submissions/submission.visitor.ts
@@ -12,49 +12,6 @@ import {
   SortingQuestionSubmissionInputDTO,
   TextQuestionSubmissionInputDTO,
 } from './dto/submission';
-
-export abstract class SubmissionInputVisitor<R> {
-  protected abstract visitSingleChoiceQuestionSubmissionInput(
-    singleChoiceQuestionSubmissionInput: SingleChoiceQuestionSubmissionInputDTO,
-  ): R;
-
-  protected abstract visitMultipleChoiceQuestionSubmissionInput(
-    multipleChoiceQuestionSubmissionInput: MultipleChoiceQuestionSubmissionInputDTO,
-  ): R;
-
-  protected abstract visitSortingQuestionSubmissionInput(
-    sortingQuestionSubmissionInputDTO: SortingQuestionSubmissionInputDTO,
-  ): R;
-
-  protected abstract visitTextQuestionSubmissionInput(
-    textQuestionSubmissionInputDTO: TextQuestionSubmissionInputDTO,
-  ): R;
-
-  visit(questionSubmissionInput: QuestionSubmissionInputDTO): R {
-    if (questionSubmissionInput.singleChoiceQuestionSubmissionInput != null) {
-      return this.visitSingleChoiceQuestionSubmissionInput(
-        questionSubmissionInput.singleChoiceQuestionSubmissionInput,
-      );
-    } else if (
-      questionSubmissionInput.multipleChoiceQuestionSubmissionInput != null
-    ) {
-      return this.visitMultipleChoiceQuestionSubmissionInput(
-        questionSubmissionInput.multipleChoiceQuestionSubmissionInput,
-      );
-    } else if (questionSubmissionInput.sortingQuestionSubmissionInput != null) {
-      return this.visitSortingQuestionSubmissionInput(
-        questionSubmissionInput.sortingQuestionSubmissionInput,
-      );
-    } else if (questionSubmissionInput.textQuestionSubmissionInput != null) {
-      return this.visitTextQuestionSubmissionInput(
-        questionSubmissionInput.textQuestionSubmissionInput,
-      );
-    } else {
-      throw new Error('Missing or unexpected question submission input');
-    }
-  }
-}
-
 export abstract class QuestionSubmissionVisitor<R> {
   protected abstract visitSingleChoice(
     singleChoiceQuestionSubmissionInput: SingleChoiceQuestionSubmissionInputDTO,
