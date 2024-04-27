a2853ef207a0aedc1c1a7c1bb380a2096a497386
Mateusz Okulus <mmokulus@gmail.com>
2024-04-27 10:18:28 +0200

    Add quizzes tests

--- src/quizzes/quizzes.resolver.spec.ts
+++ src/quizzes/quizzes.resolver.spec.ts
@@ -1,19 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuizzesResolver } from './quizzes.resolver';
 import { QuizzesService } from './quizzes.service';
+import { CreateQuizInputDTO, UpdateQuizInputDTO } from './dto/quiz';
+import { QuestionInputValidationService } from '../questions/question.validation';
 
 describe('QuizzesResolver', () => {
   let resolver: QuizzesResolver;
+  let service: QuizzesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuizzesResolver, QuizzesService],
+      providers: [
+        QuizzesResolver,
+        QuestionInputValidationService,
+        {
+          provide: QuizzesService,
+          useValue: {
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+            findAll: jest.fn(),
+            findById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<QuizzesResolver>(QuizzesResolver);
+    service = module.get<QuizzesService>(QuizzesService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('createQuiz', () => {
+    it('should call create method of service', async () => {
+      const createQuizInput = new CreateQuizInputDTO();
+      await resolver.create(createQuizInput);
+      expect(service.create).toHaveBeenCalledWith(createQuizInput);
+    });
+  });
+  describe('updateQuiz', () => {
+    it('should call update method of service', async () => {
+      const updateQuizInput = new UpdateQuizInputDTO();
+      await resolver.update(updateQuizInput);
+      expect(service.update).toHaveBeenCalledWith(updateQuizInput);
+    });
+  });
+
+  describe('remove', () => {
+    it('should call remove method of service', async () => {
+      const id = '1';
+      await resolver.remove(id);
+      expect(service.remove).toHaveBeenCalledWith(+id);
+    });
+  });
+
+  describe('quizzes', () => {
+    it('should call findAll method of service', async () => {
+      await resolver.quizzes();
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('quiz', () => {
+    it('should call findById method of service with correct input', async () => {
+      const id = '1';
+      await resolver.quiz(id);
+      expect(service.findById).toHaveBeenCalledWith(+id);
+    });
   });
 });
--- src/quizzes/quizzes.service.spec.ts
+++ src/quizzes/quizzes.service.spec.ts
@@ -1,18 +1,210 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuizzesService } from './quizzes.service';
+import { Quiz } from './entities/quiz.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { QuestionsService } from '../questions/questions.service';
+import { QuestionConverterService } from '../questions/question.convert';
+import { CreateQuizInputDTO, UpdateQuizInputDTO } from './dto/quiz';
+import { Repository } from 'typeorm';
+import {
+  QuestionInputDTO,
+  SingleChoiceQuestionInputDTO,
+} from '../questions/dto/question';
+import { QuestionInputValidationService } from '../questions/question.validation';
+import {
+  Question,
+  SingleChoiceQuestion,
+} from '../questions/entitites/question.entity';
+
+function makeSingleChoiceQuestionInput(): QuestionInputDTO {
+  const questionInput = new QuestionInputDTO();
+  const singleChoiceQuestionInput = new SingleChoiceQuestionInputDTO();
+  singleChoiceQuestionInput.prompt = 'Single choice question';
+  singleChoiceQuestionInput.answers = ['A', 'B'];
+  singleChoiceQuestionInput.correctAnswer = 'A';
+  questionInput.singleChoiceQuestionInput = singleChoiceQuestionInput;
+  return questionInput;
+}
 
 describe('QuizzesService', () => {
-  let service: QuizzesService;
+  let quizzesService: QuizzesService;
+  let questionsService: QuestionsService;
+  let repository: Repository<Quiz>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuizzesService],
+      providers: [
+        QuizzesService,
+        {
+          provide: getRepositoryToken(Quiz),
+          useValue: {
+            save: jest.fn((a: Quiz) => {
+              a.questions = a.questions?.map((q, i) =>
+                Object.assign(q, { id: i }),
+              );
+              return Object.assign(a, { id: 0 });
+            }),
+            findOne: jest.fn(() => new Quiz()),
+            find: jest.fn(),
+            remove: jest.fn(() => Object.assign(new Quiz(), { id: 0 })),
+          },
+        },
+        QuestionsService,
+        QuestionConverterService,
+        {
+          provide: QuestionInputValidationService,
+          useValue: {
+            validate: jest.fn((a) => a),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<QuizzesService>(QuizzesService);
+    quizzesService = module.get<QuizzesService>(QuizzesService);
+    questionsService = module.get<QuestionsService>(QuestionsService);
+    repository = module.get(getRepositoryToken(Quiz));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(quizzesService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should save in repository', async () => {
+      const createQuizInput = new CreateQuizInputDTO();
+      createQuizInput.name = 'Quiz';
+      createQuizInput.questions = [];
+      await quizzesService.create(createQuizInput);
+      expect(repository.save).toHaveBeenCalled();
+    });
+
+    it('should use makeEntity correct number of times', async () => {
+      const createQuizInput = new CreateQuizInputDTO();
+      createQuizInput.name = 'Quiz';
+      createQuizInput.questions = [
+        makeSingleChoiceQuestionInput(),
+        makeSingleChoiceQuestionInput(),
+      ];
+      const question = new Question();
+      question.id = 0;
+      const singleChoiceQuestion = new SingleChoiceQuestion();
+      singleChoiceQuestion.id = 0;
+      question.singleChoiceQuestion = singleChoiceQuestion;
+      jest
+        .spyOn(questionsService, 'makeEntity')
+        .mockImplementation(() => question);
+      await quizzesService.create(createQuizInput);
+      expect(questionsService.makeEntity).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe('update', () => {
+    it('should use findOne from repository', async () => {
+      const updateQuizInput = new UpdateQuizInputDTO();
+      await quizzesService.update(updateQuizInput);
+      expect(repository.findOne).toHaveBeenCalled();
+    });
+
+    it('should use save from repository', async () => {
+      const updateQuizInput = new UpdateQuizInputDTO();
+      await quizzesService.update(updateQuizInput);
+      expect(repository.save).toHaveBeenCalled();
+    });
+
+    it('should return null for nonexistent quiz', async () => {
+      const updateQuizInput = new UpdateQuizInputDTO();
+      jest.spyOn(repository, 'findOne').mockReturnValue(Promise.resolve(null));
+      const result = await quizzesService.update(updateQuizInput);
+      expect(result).toBeNull();
+    });
+
+    it('should update name when it is set', async () => {
+      const updateQuizInput = new UpdateQuizInputDTO();
+      updateQuizInput.id = '0';
+      updateQuizInput.name = 'New name';
+      const oldQuiz = new Quiz();
+      oldQuiz.id = 0;
+      oldQuiz.name = 'Old name';
+      jest
+        .spyOn(repository, 'findOne')
+        .mockReturnValue(Promise.resolve(oldQuiz));
+      const result = await quizzesService.update(updateQuizInput);
+      expect(result).not.toBeNull();
+      expect(result?.name).toStrictEqual('New name');
+    });
+
+    it('should update questions when they are set', async () => {
+      const updateQuizInput = new UpdateQuizInputDTO();
+      updateQuizInput.id = '0';
+      updateQuizInput.questions = [
+        Object.assign(new QuestionInputDTO(), {
+          singleChoiceQuestionInput: Object.assign(
+            new SingleChoiceQuestionInputDTO(),
+            {
+              prompt: 'prompt',
+            },
+          ),
+        }),
+      ];
+      const oldQuiz = new Quiz();
+      oldQuiz.id = 0;
+      oldQuiz.name = 'Old name';
+      oldQuiz.questions = [];
+      jest
+        .spyOn(repository, 'findOne')
+        .mockReturnValue(Promise.resolve(oldQuiz));
+      const result = await quizzesService.update(updateQuizInput);
+      expect(result?.questions[0].prompt).toStrictEqual('prompt');
+      expect(result?.name).toStrictEqual('Old name');
+    });
+  });
+
+  describe('remove', () => {
+    it('should use remove', async () => {
+      await quizzesService.remove(0);
+      expect(repository.remove).toHaveBeenCalled();
+    });
+    it('should return null for missing id', async () => {
+      jest.spyOn(repository, 'findOne').mockReturnValue(Promise.resolve(null));
+      const result = await quizzesService.remove(0);
+      expect(result).toBeNull();
+    });
+
+    it('should return removed id', async () => {
+      jest
+        .spyOn(repository, 'findOne')
+        .mockReturnValue(Promise.resolve(Object.assign(new Quiz(), { id: 0 })));
+      const result = await quizzesService.remove(0);
+      expect(result?.id).toEqual(0);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should use findAll from repository', async () => {
+      const quizzes = Array.from({ length: 4 }, (_, i) =>
+        Object.assign(new Quiz(), { id: i }),
+      );
+      jest.spyOn(repository, 'find').mockReturnValue(Promise.resolve(quizzes));
+      const result = await quizzesService.findAll();
+      expect(repository.find).toHaveBeenCalled();
+      expect(result?.length).toEqual(4);
+      for (let i = 0; i < 4; ++i) expect(result[i]?.id).toEqual(i);
+    });
+  });
+
+  describe('findById', () => {
+    it('should use findOne from repository', async () => {
+      const quiz = Object.assign(new Quiz(), { id: 3 });
+      jest.mocked(repository.findOne).mockReturnValue(Promise.resolve(quiz));
+      const result = await quizzesService.findById(3);
+      expect(result).not.toBeNull();
+      expect(result?.id).toEqual(3);
+    });
+
+    it('should return null for nonexistent quiz', async () => {
+      jest.mocked(repository.findOne).mockReturnValue(Promise.resolve(null));
+      const result = await quizzesService.findById(3);
+      expect(result).toBeNull();
+    });
   });
 });
